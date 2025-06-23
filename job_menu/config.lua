1ad6b30e03372677d5449ef3991144c431bb4725
KP SCRIPT <thorgotasoo@gmail.com>
2025-06-23 11:33:13 +0530

    config.lua

--- job_menu/config.lua
+++ job_menu/config.lua
@@ -1,1 +1,37 @@
+Config = {}
 
+Config.JobName = "sgod"
+Config.RadioChannel = 26
+
+Config.Outfits = {
+    [0] = { -- Shadow Guard
+        male = {
+            ['tshirt_1'] = 15, ['torso_1'] = 243, ['arms'] = 31,
+            ['pants_1'] = 87, ['shoes_1'] = 25, ['helmet_1'] = -1
+        },
+        female = {
+            ['tshirt_1'] = 3, ['torso_1'] = 255, ['arms'] = 14,
+            ['pants_1'] = 50, ['shoes_1'] = 6, ['helmet_1'] = -1
+        }
+    },
+    [1] = { -- Shadow Cat
+        male = {
+            ['tshirt_1'] = 3, ['torso_1'] = 278, ['arms'] = 32,
+            ['pants_1'] = 97, ['shoes_1'] = 10, ['helmet_1'] = 116
+        },
+        female = {
+            ['tshirt_1'] = 2, ['torso_1'] = 264, ['arms'] = 15,
+            ['pants_1'] = 61, ['shoes_1'] = 3, ['helmet_1'] = 119
+        }
+    },
+    [2] = { -- Shadow Leader
+        male = {
+            ['tshirt_1'] = 0, ['torso_1'] = 294, ['arms'] = 34,
+            ['pants_1'] = 95, ['shoes_1'] = 20, ['helmet_1'] = 114
+        },
+        female = {
+            ['tshirt_1'] = 1, ['torso_1'] = 270, ['arms'] = 20,
+            ['pants_1'] = 63, ['shoes_1'] = 9, ['helmet_1'] = 115
+        }
+    }
+}
